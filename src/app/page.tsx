2efd727f5a28a1ded19dea47aaa7052c66eb1787
Aman Prajapati <aman.prajapati3000@gmail.com>
2025-09-10 12:00:47 +0530

    feat : activity page for the tiemr and displaying logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import ActivityPage from "@/components/logs/timer/logPage";
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: "Dailylogger",
+    description: "Start the timer and log your activities"
+}
+
 export default function () {
-  return <div></div>
+  return <ActivityPage />
 }
