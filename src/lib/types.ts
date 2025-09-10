9481529fdb01f2b7ce8e928e30d3b836e75c3a04
Aman Prajapati <aman.prajapati3000@gmail.com>
2025-09-10 12:05:59 +0530

    chore :  adde types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,10 @@
 export type Log = {
-    duration: string;
     category: string;
-    startedAt: Date
+    startedAt: Date;
+    finishedAt: Date | null;
+    status?: string;
+}
+export enum Sort {
+    desc = "desc",
+    asc = "asc"
 }
